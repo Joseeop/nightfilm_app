7936726865b32787b0ad83c9632bd4f511c8cccf
Jose Ona <onapinedajose@gmail.com>
2023-07-09 13:12:54 +0200

    Añadido mensaje si no tienes aún películas favoritas

--- lib/presentation/views/movies/favorite_view.dart
+++ lib/presentation/views/movies/favorite_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:nightfilm/presentation/providers/storage/favorites_movies_provider.dart';
 import 'package:nightfilm/presentation/widgets/movies/movie_masonry.dart';
 
@@ -42,6 +43,26 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
     //Listamos el mapa de películas
     final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    if (favoritesMovies.isEmpty){
+      final colors = Theme.of(context).colorScheme;
+
+      return Center(
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.favorite_border_sharp ,size: 60, color: colors.primary),
+          Text('¡Vaaaaaya!', style: TextStyle(fontSize: 30, color: colors.primary)),
+          const Text('No tienes películas favoritas',style: TextStyle(fontSize: 20,color: Colors.black45),),
+          const SizedBox(height: 20,),
+          FilledButton.tonal(
+            onPressed: () => context.go('/home/0'), 
+            child: const Text('Busca tus películas favoritas'))
+        ],
+       )
+      );
+    }
 
 
     return Scaffold(
