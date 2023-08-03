81289a967ce17f87ab209e274b9932a8e3e02f34
Jose Ona <onapinedajose@gmail.com>
2023-08-03 12:41:26 +0200

    Reorganización

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,13 +1,7 @@
 export 'package:nightfilm/presentation/widgets/shared/full_screen_loader.dart';
-
 export 'package:nightfilm/presentation/widgets/movies/movie_horizontal_listview.dart';
-
 export 'package:nightfilm/presentation/widgets/movies/movies_slideshow.dart';
-
 export 'package:nightfilm/presentation/widgets/shared/custom_appbar.dart';
-
 export 'package:nightfilm/presentation/widgets/shared/custom_bottom_navigation.dart';
-
 export 'movies/movie_masonry.dart';
-
 export 'movies/movie_poster_link.dart';
